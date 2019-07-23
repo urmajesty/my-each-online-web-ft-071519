44d16e8f2f9574ae53d874416dbd3933dba412a9
Chyrelle Lewis <chyrellelewis@gmail.com>
2019-07-23 19:14:53 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,11 @@
-def my_each # put argument(s) here
-  # code here
+def my_each(array)
+  i = l
+  while i < array.length
+  my_each(items) do |item|
+    p item (items[i = i + 1])
+  yield array[i]
+  
+end
+return my_each
+end
 end
