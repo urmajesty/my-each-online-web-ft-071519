4ff700731582b55ab2a726694eca6eb0a652b0a1
Chyrelle Lewis <chyrellelewis@gmail.com>
2019-07-23 19:23:48 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -2,13 +2,12 @@ def my_each(array)
   i = 0
   while i < array.length
  yield array[i]
-  i
+  i += 1
  
  
  # my_each(items) do |item|
    # p item (items[i = i + 1])
  
 end
-return my_each
-end
+return array
 end
